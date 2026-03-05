f16907e129fcc4015aeb391d6e6d4babf7a82d2f
serginavarra1 <serginavarrapares@gmail.com>
2026-03-05 19:49:39 +0100

    commit before big changes

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  envDir: '../../',
+  envDir: '.',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
